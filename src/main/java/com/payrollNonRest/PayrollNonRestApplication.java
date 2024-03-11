636f62459eb75de22e69849c7fc69bc5a13f17c5
Nesrin Vural Gedik <nesrinvural@gmail.com>
2024-03-11 14:38:35 -0400

    first commit

---
+++ src/main/java/com/payrollNonRest/PayrollNonRestApplication.java
@@ -0,0 +1,13 @@
+package com.payrollNonRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollNonRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollNonRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payrollNonRest/PayrollNonRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payrollNonRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollNonRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
